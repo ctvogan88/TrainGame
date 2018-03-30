958f823d316287c5847092f3a301fd2c3cd8aaef
ctvogan88 <clayton.vogan@gmail.com>
2018-03-30 01:00:53 -0700

    working schedule

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,7 @@ console.log("DB: " + database);
 var name = "clayton";
 var destination = "";
 var initialTime = 0;
-var frequency = 0;
+var frequency = 5;
 var nextTime = 0;
 var deltaTime = 0;
 
@@ -55,20 +55,18 @@ $("#submit").on("click", function (event) {
     }
 });
 
-
-
-
 var createRow = function (data) {
     //----
 
     if (!data) {
         alert("no more data");
     } else {
-        // Assumptions
-        var tFrequency = 3;
+        
+        // get the arrival frequency from Firebase
+        var freq = data.frequency;
 
-        // define initial time variable with user input
-        var firstTime = "03:30";
+        // define initial time from Firebase
+        var firstTime = data.initialTime;
 
         // First Time (pushed back 1 year to make sure it comes before current time)
         var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
@@ -76,61 +74,70 @@ var createRow = function (data) {
 
         // Current Time
         var currentTime = moment();
-        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+        var newTime = moment(currentTime).format("hh:mm");
 
         // Difference between the times
         var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
         console.log("DIFFERENCE IN TIME: " + diffTime);
 
         // Time apart (remainder)
-        var tRemainder = diffTime % tFrequency;
+        var tRemainder = diffTime % freq;
         console.log(tRemainder);
 
         // Minute Until Train
-        var tMinutesTillTrain = tFrequency - tRemainder;
-        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+        var delta = freq - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + delta);
 
         // Next Train
-        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+        console.log(firstTime + " ----- " + newTime);
+
+        if (firstTime < newTime) {
+        var nextArr = moment().add(delta, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextArr).format("hh:mm"));
+        } else {var nextArr = "Train has not begun service yet!"};
 
 
         // sets variables for html elements to be added
         var tBody = $("tbody");
         var tRow = $("<tr>");
 
-
         // define variables for either 
         var name = (data.name || name);
         var destination = (data.destination || destination);
         var initialTime = (data.initialTime || initialTime);
         var frequency = (data.frequency || frequency);
+      
         //Math.floor(moment(today).diff(moment(compareDate), 'months', true));
         //console.log("name: " + name + " destination: " + destination + " intitial time: " + initialTime + " frequency: " + frequency);
 
+        // create some variables to assign either data from Firebase or input data to table data elements
         var nameTd = $("<td>").text(data.name || name);
         var destinationTd = $("<td>").text(data.destination || destination);
         var initialTimeTd = $("<td>").text(data.initialTime || initialTime);
         var frequencyTd = $("<td>").text(data.frequency || frequency);
-
+        var nextArrTd = $("<td>").text(nextArr);
+        var deltaTd = $("<td>").text(delta);
         /*  if (isNaN(convertedDate)) {
              var workedTd = $("<td>").text("pending application review");
          } else { var workedTd = $("<td>").text(convertedDate); } */
 
-        //var nextArrTd = $("<td>").text("");
-
         //var deltaTd = $("<td>").text("");//JSON.stringify(data.empMonthlyRate || monthlyRate);
 
-
-
         // adds the table data elements to the table row element AKA tRow
-        tRow.append(nameTd, destinationTd, initialTimeTd, frequencyTd);
-        console.log(tRow);
+        tRow.append(nameTd, destinationTd, initialTimeTd, frequencyTd, nextArrTd, deltaTd);
         // adds the tRow variable to the table body AKA tBody
         tBody.append(tRow);
-        console.log(tBody);
     };
 };
+
+$("tr").on("click", function (event) {
+
+    // not really sure what this does
+    event.preventDefault();
+
+    alert("you clicked a table row!")
+
+});
 
 database.ref().on("child_added", function (snapshot) {
     console.log("he shoots he scores (the snapshot): " + snapshot.val());
